ac0e3ae20af77a87b4b1eb1ddacb29062dd2903b
Savishkanishen <savishkanishen9@gmail.com>
2025-12-02 20:45:25 +0530

    add-API

--- Martin/src/main.jsx
+++ Martin/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ContextProvider, { Context } from './context/context.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ContextProvider>
     <App />
-  </StrictMode>,
+  </ContextProvider>,
 )
